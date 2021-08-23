159fb41d5084f86a6cecc9b2fca8466f2ec3223e
Petar Popovski <ppopovski53@gmail.com>
2021-08-23 22:00:20 +0200

    Commiting UnitTests project xUnit

---
+++ TicketShop.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace TicketShop.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+
+
+        }
+    }
+}
